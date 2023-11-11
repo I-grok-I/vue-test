bf9f1fa0d91f29c6071242cdf3eabb2c7288fec2
I-Grok-I <17bltavbulaev@gmail.com>
2023-11-11 18:17:38 +0500

    x

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,11 @@
 <template>
-  <div>
-    <form @submit.prevent="createTodo(todo.title, todo.description)">
-      <input v-model="todo.title" placeholder="Название">
-      <input v-model="todo.description" placeholder="Описание">
-      <my-btn type="submit" :disabled="!todo.title || !todo.description"
-        :class="{ disabled_btn: !todo.title || !todo.description }">Создать</my-btn>
-    </form>
-
-
     <div>
-      <div class="todo_item" v-for="todo in todoList" :key="todo.id">
-        <div>
-          <h4>{{ todo.id + '. ' + todo.title }}</h4>
-          <p>{{ todo.description }}</p>
-        </div>
-        <my-btn @click="deleteTodo(todo.id)">Удалить</my-btn>
-      </div>
+        Привет!
     </div>
-  </div>
 </template>
 
-<script setup>
-import useTodos from '@/hooks/useTodos.js'
-
-const { todo, todoList, createTodo, deleteTodo } = useTodos()
-
-
-
-
-
-
-
-
+<script setup lang="ts">
 
 </script>
 
-<style lang="css">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-.todo_item {
-  display: flex;
-  justify-content: space-between;
-  border: 2px solid black;
-  margin: 10px;
-  padding: 5px;
-}
-</style>
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+
+const app = createApp(App)
+
+// app.use(createPinia())
+// app.use(router)
+// uis.forEach(ui => app.component(ui.name, ui))
+
+
+app.mount('#app')
