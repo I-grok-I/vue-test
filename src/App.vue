78e9a92dadef961b96a73e1ca9dbdc45c7f192b3
I-Grok-I <17bltavbulaev@gmail.com>
2023-11-19 23:51:02 +0500

    ui check

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app">
-        <AppearanceProvider theme="apple" colorScheme="dark" platform="macos">
+        <AppearanceProvider>
             <Switch />
             <RadioButton />
 
